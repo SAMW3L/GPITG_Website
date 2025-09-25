9af8e5d95fef84ee68c478ebf4e6f31cab24ea82
SAMW3L <samuelsarah678@gmail.com>
2025-09-25 16:42:14 +0300

    fix proxy

--- vite.config.ts
+++ vite.config.ts
@@ -10,8 +10,16 @@ export default defineConfig({
   server: {
     port:3000,
     open:true,
+     proxy: {
+      '/api': {
+        target: 'http://41.188.172.204:30017',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      },
   }
   
 });
+
+
 
 
